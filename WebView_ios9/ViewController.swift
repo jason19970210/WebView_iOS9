c602c3d3bc46122f4de82c435262958e51356bca
jason19970210 <jason19970210@gmail.com>
2018-02-23 19:36:32 +0800

    20180223 Up to Date

--- WebView_ios9/ViewController.swift
+++ WebView_ios9/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let WHGSURL = URL(string: "http://iot.ykchen.idv.tw")
+        let WHGSURLRequest = URLRequest(url: WHGSURL!)
+        webView.loadRequest(WHGSURLRequest)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
